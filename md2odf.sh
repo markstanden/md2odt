b7c1ca8d787ec3b5b1fb67711bb45580f358d464
Mark Standen <mark@markstanden.uk>
2021-03-07 16:16:30 +0000

    added script

---
+++ md2odf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+convert ()
+{
+			pandoc $1 \
+			-f gfm \
+			--columns=10 \
+			-t odt \
+			-o $2
+}
+
+usage ()
+{
+	echo "Converts from git-flavoured Markdown to ODT"
+	echo "I can't stand word processors, so this means I don't have to mess around to0 much"
+	echo " Usage: $1 infile [outfile]"
+}
+
+IFILE="$1"
+
+# exit if an input file is not provided
+if [ -z "$IFILE" ]; then
+	usage "$0"
+	exit 1
+fi
+
+# Output filename default is "converted.odt"
+if [ ! -z "$2" ]; then
+	OFILE="$2"
+else
+	OFILE="converted.odt"
+fi
+
+convert "$IFILE" "$OFILE"

cdad2124ccfa1c9326aba5c0bad2dbce95e826f7
Mark Standen <mark@markstanden.uk>
2021-03-07 16:21:56 +0000

    added comments to .sh

--- md2odf.sh
+++ md2odf.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# A script to convert github flavoured markdown into ODT format
+# required to submit coursework as a docx file, I open and check the
+# ODT file in libreoffice before saving as a docx and submitting
+
+# I created this because I always forget the command and it will
+# presumably remain fairly constant
+
+# you can do what you want with this
+# thank pandoc, not me.
+
 convert ()
 {
 			pandoc $1 \
